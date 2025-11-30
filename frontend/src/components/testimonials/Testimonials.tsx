07f38de36f171fce07ac469b8a57c39a0c367990
mizanur2734 <mizanurbb2791@gmail.com>
2025-11-30 12:23:46 +0600

    create a Testimonials section

---
+++ frontend/src/components/testimonials/Testimonials.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Testimonials = () => {
+    return (
+        <div>
+            this Testimonials section
+        </div>
+    );
+};
+
+export default Testimonials;
