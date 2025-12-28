100094fb962a15250e66db1917e6bbf4a2932846
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2025-12-28 17:58:17 +0600

    fix the outline of input field name education level

--- frontend/src/app/dashboard/user/join-as-a-Problem-Solver/page.tsx
+++ frontend/src/app/dashboard/user/join-as-a-Problem-Solver/page.tsx
@@ -1194,7 +1194,7 @@ export default function ApplyProblemSolver() {
                             setValue('educationLevel', customEducationLevel, { shouldValidate: true });
                           }
                         }}
-                        className="w-full px-3 xs:px-4 py-2 xs:py-2.5 sm:py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#2a7d2f] focus:border-[#2a7d2f] transition-colors text-sm xs:text-base"
+                        className="w-full px-3 xs:px-4 py-2 xs:py-2.5 sm:py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#2a7d2f] focus:border-[#2a7d2f] transition-colors text-sm xs:text-base outline-none"
                       >
                         {educationLevelOptions.map((option) => (
                           <option key={option.value} value={option.value}>
