7c70cdb40287aad4ddde41ada4cfb44c76eb764d
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2025-12-28 17:35:45 +0600

    fix the make report form description section

--- frontend/src/app/dashboard/user/reports/new/page.tsx
+++ frontend/src/app/dashboard/user/reports/new/page.tsx
@@ -637,7 +637,7 @@ export default function NewReportPage() {
                   })}
                   placeholder="Describe the issue clearly... (minimum 20 characters)"
                   rows={4}
-                  className={`w-full px-3 xs:px-4 py-2 xs:py-2.5 sm:py-3 border-2 rounded-lg resize-none focus:ring-2 focus:ring-primary focus:border-primary transition text-sm xs:text-base ${errors.description ? "border-red-500 bg-red-50" : "border-gray-300 hover:border-gray-400"
+                  className={`w-full px-3 xs:px-4 py-2 xs:py-2.5 sm:py-3 border-2 rounded-lg resize-none focus:ring-2 focus:ring-primary focus:border-primary transition text-sm xs:text-base ${errors.description ? "border-red-500 bg-red-50" : "border-gray-300 hover:border-gray-400 outline-none"
                     }`}
                 ></textarea>
                 {errors.description && (
